b76d311d4517090c1e1a007d9d3090f8a5d028c1
JP-3 <johnprout16@gmail.com>
2023-10-13 19:42:19 -0500

    checkin

---
+++ WebElementExtensions/FindExtensions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Protractor;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace WebExtensions
+{
+    public class ByExtension : By
+    {
+        /// <summary>
+        /// This is only for Angular applications as it's using the prefixes for angular binding
+        /// </summary>
+        /// <param name="tagName"></param>
+        /// <param name="attribute"></param>
+        /// <param name="searchString"></param>
+        /// <returns></returns>
+        public static By Find(string tagName, string attribute, string searchString)
+        {
+            string find = @"
+        var using = arguments[0] || document;
+        var tagName = arguments[1];
+        var identifier = arguments[2];
+        var searchText = arguments[3];
+        var prefixes = ['ng-', 'ng_', 'data-ng-', 'x-ng-', 'ng\\:', ''];
+        for (var p = 0; p < prefixes.length; ++p) 
+        {
+            var selector =  tagName + '[' + prefixes[p] + identifier + '=""' + searchText + '""]';
+            var inputs = using.querySelectorAll(selector);
+            if (inputs.length) 
+            {
+                return inputs;
+            }
+        }";
+            //return By;
+            return new JavaScriptBy(find, tagName.ToString(), attribute, searchString);
+        }
+
+        public static By LabelText(string text)
+        {
+            string FindByLabelText =
+          "var using = arguments[0]  || document; " +
+          "var searchText = arguments[1];" +
+          "var elements = using.querySelectorAll('label');" +
+          "var matches = [];  " +
+          "for (var i = 0; i < elements.length; ++i) {    " +
+          "var element = elements[i];    " +
+          "var elementText;    " +
+          "if (element.tagName.toLowerCase() == 'label') {      " +
+          "elementText = element.innerText || element.textContent;    } " +
+          "else {      elementText = element.value;    }    " +
+          "if (elementText.trim() === searchText) {      " +
+          "matches.push(element);    }  }  return matches; ";
+
+            return new JavaScriptBy(FindByLabelText, text);
+        }
+
+        public static By Button(string buttonText)
+        {
+            string FindButton =
+        "var using = arguments[0]  || document; var searchText = arguments[1];" +
+        "var elements = using.querySelectorAll('button, input[type=\"button\"], " +
+        "input[type=\"submit\"]');  " +
+        "var matches = [];  " +
+        "for (var i = 0; i < elements.length; ++i) {    " +
+        "var element = elements[i];    " +
+        "var elementText;    " +
+        "if (element.tagName.toLowerCase() == 'button') {      " +
+        "elementText = element.innerText || element.textContent;    } " +
+        "else {      elementText = element.value;    }    " +
+        "if (elementText.trim() === searchText) {      " +
+        "matches.push(element);    }  }  return matches; ";
+
+            return new JavaScriptBy(FindButton, buttonText);
+        }
+
+        public static By ButtonPartialText(string buttonText)
+        {
+            string FindButtonByPartialText =
+        "var using = arguments[0]  || document; var searchText = arguments[1];" +
+        "var elements = using.querySelectorAll('button, input[type=\"button\"], " +
+        "input[type=\"submit\"]');  " +
+        "var matches = [];  " +
+        "for (var i = 0; i < elements.length; ++i) {    " +
+        "var element = elements[i];    " +
+        "var elementText;    " +
+        "if (element.tagName.toLowerCase() == 'button') {      " +
+        "elementText = element.innerText || element.textContent;    } " +
+        "else {      elementText = element.value;    }    " +
+        "if (elementText.indexOf(searchText)  > -1) {      " +
+        "matches.push(element);    }  }  return matches; ";
+
+            return new JavaScriptBy(FindButtonByPartialText, buttonText);
+        }
+    }
+}
---
+++ WebElementExtensions/IWebElementExtensions.cs
@@ -0,0 +1,83 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebExtensions
+{
+    public static class IWebElementExtensions
+    {
+        public static void XEnterTextBox(this IWebElement element, string textToEnter)
+        {
+            element.Clear();
+            element.SendKeys(textToEnter);
+        }
+
+        /// <summary>
+        /// Need to pass in an ENUM to use this, which forces you to not have hardcoded values in your tests. 
+        /// You can call it like this "XSelectCombobox<MyEnums>(MyEnums.Chicago.ToString));"
+        /// If your enum has a space or special characters add it to your enum as such "[EnumMember(Value = "IT-7650")] IT7650 = 1,"
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="element"></param>
+        /// <param name="valueToSelect"></param>
+        public static void XSelectCombobox<T>(this IWebElement element, string valueToSelect) where T : struct, IConvertible
+        {
+            bool valueSet = false;
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T must be an enumerated type");
+            }
+
+            var enumType = typeof(T);
+            EnumMemberAttribute enumMemberAttribute = null;
+            SelectElement clickThis;
+            string value;
+            foreach (var name in Enum.GetNames(enumType))
+            {
+                try
+                {
+                    enumMemberAttribute = ((EnumMemberAttribute[])enumType.GetField(name).GetCustomAttributes(typeof(EnumMemberAttribute), true)).Single();
+                    value = enumMemberAttribute.Value;
+                    value = Regex.Replace(value, @"[^A-Za-z0-9]+", "");
+                    if (value == valueToSelect)
+                    {
+                        clickThis = new SelectElement(element);
+                        clickThis.SelectByText(enumMemberAttribute.Value);
+                        valueSet = true;
+                        break;
+                    }
+                }
+                catch { }
+            }
+
+            //If value hasn't been set it must not have attribute
+            if (!valueSet)
+            {
+                clickThis = new SelectElement(element);
+                clickThis.SelectByText(valueToSelect);
+            }
+        }
+
+        public static void XEnterComboSimple(this IWebElement element, string valueToSelect)
+        {
+            SelectElement clickThis = new SelectElement(element);
+            clickThis.SelectByText(valueToSelect);
+        }
+
+
+        public static void XCompareComboValues(this IList<IWebElement> elements, List<string> baseList)
+        {
+            for (int i = 0; i < baseList.Count; i++)
+            {
+                int b = string.Compare(elements[i].Text, baseList[i]);
+            }
+        }
+    }
+}
+
